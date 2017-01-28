d8363043c1e9c9213ab0dad0db65d4656c255790
Thetudors <152804004@ogr.cbu.edu.tr>
2017-01-28 20:45:18 +0200

    First Commit
    First Commit

---
+++ Unity_Ball_Game/Assets/Scripts/Ball_Move.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Ball_Move : MonoBehaviour {
+    public static Ball_Move instance;
+    bool isContact;
+    Rigidbody Ball_rb;
+
+    
+
+	// Use this for initialization
+	void Start () {
+
+        Ball_rb = gameObject.GetComponent<Rigidbody>();
+        instance = this;
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void FixedUpdate()
+    {
+        
+
+        Ball_Movement(10);
+        Ball_Jump(500);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            isContact = true;   
+        }
+        if(collision.gameObject.tag == "Block")
+        {
+            Cam_Follow.Instance.Shake(0.1f, 0.5f);
+            
+        }
+        if (collision.gameObject.tag == "Finish")
+        {
+            Debug.Log("Bitirdin");
+        }
+
+    }
+
+
+    void Ball_Jump(float Jump_Speed)
+    {
+        if (isContact)
+        {
+            Ball_rb.AddForce(new Vector3(0, Jump_Speed));
+            isContact = false;
+        }
+    }
+    
+    void Ball_Movement(float speed)
+    {
+       
+        float Move_Horizontal;
+        Move_Horizontal = Input.GetAxis("Horizontal");
+        Ball_rb.AddForce(new Vector3(Move_Horizontal * speed, 0, 0));
+    }
+
+    public void GameOver()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.buildIndex);
+    }
+        
+
+
+    
+
+}
---
+++ Unity_Ball_Game/Assets/Scripts/Cam_Follow.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cam_Follow : MonoBehaviour {
+
+    public static Cam_Follow Instance;
+
+    public Transform Target;
+    private float _amplitude = 0.1f;//Genlik miktarı
+    
+    private bool isShaking = false;//Sallanıyor mu ?
+    private Vector3 initialPosition;//Başlangıç pozisyonu
+
+    Vector3 offset = new Vector3(0, 0, -10);
+	void Start () {
+        Instance = this;
+        
+    }
+
+    public void Shake(float amplitude, float duration)//Duration = süre
+    {
+        initialPosition = transform.localPosition;
+        isShaking = true;
+        _amplitude = amplitude;
+        CancelInvoke();
+        Invoke("StopShaking", duration);
+    }
+
+    public void StopShaking()
+    {
+        Ball_Move.instance.GameOver();
+        isShaking = false;
+    }
+
+    void Update()
+    {
+        if(isShaking)
+        {
+            transform.localPosition = initialPosition + Random.insideUnitSphere * _amplitude;
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if(!isShaking)
+        {
+            Vector3 desiredPosition = new Vector3(Target.transform.position.x, 0, 0) + offset;
+            transform.position = desiredPosition;
+        }
+       
+	
+	}
+}
