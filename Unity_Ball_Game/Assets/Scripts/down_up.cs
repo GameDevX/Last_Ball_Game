d0100e0a487d384648e1762fe3d4bea637b2d033
onursakalli <onursakalli97@gmail.com>
2017-02-08 23:11:30 +0300

    5.seviye animasyonlarıyla birlikte bitti

---
+++ Unity_Ball_Game/Assets/Scripts/down_up.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class down_up : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(0, -0.09f, 0);
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.name == "alt_zemin_alt")
+		{
+			Debug.Log ("degdi");
+			transform.position += new Vector3(0, 7.85f, 0);
+		}
+	}
+}
---
+++ Unity_Ball_Game/Assets/Scripts/rotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+
+	public GameObject engel1;
+	public GameObject engel2;
+	public GameObject engel3;
+	public GameObject engel4;
+	public GameObject alt_zemin;
+	public float right_speed = 35;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		engel1.transform.Rotate (new Vector3 (0, 0, Time.deltaTime * right_speed));
+		engel2.transform.Rotate (new Vector3 (0, 0, Time.deltaTime * right_speed));
+		engel3.transform.Rotate (new Vector3 (0, 0, Time.deltaTime * right_speed));
+	}
+
+
+}
